61504cf16673a8fdfae1bc2c6cc1d5a2744bb620
Yennifer017 <yenniferdelion@gmail.com>
2024-09-26 14:52:19 -0600

    RECUPERACION DE FUNCION DEL COMMIT 4a5a086200f5c5b58f961ed6981d105813b6e3a5 -- add stock products

--- DB/functions.sql
+++ DB/functions.sql
@@ -45,6 +45,25 @@ $$ LANGUAGE plpgsql;
 *********************** STOCK ***************************
 *********************************************************/
 
+--insertar en stock
+CREATE OR REPLACE FUNCTION storage.add_product_stock(
+    id_sucursal_param INTEGER,
+    id_product_param INTEGER,
+    hall_param INTEGER,
+    existences_param INTEGER
+) RETURNS VOID AS $$
+BEGIN
+    UPDATE storage.stock
+    SET existences = existences + existences_param
+    WHERE id_sucursal = id_sucursal_param AND id_product = id_product_param AND hall = hall_param;
+    IF NOT FOUND THEN --si no se realizo el update
+        INSERT INTO storage.stock(id_sucursal, id_product, hall, existences) 
+        VALUES (id_sucursal_param,id_product_param,hall_param,existences_param);
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
 CREATE OR REPLACE FUNCTION storage.update_stock(
     id_sucursal_param INTEGER,
     id_product_param INTEGER,
